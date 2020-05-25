2c0c8bf8b4d8f0b3be0f6903ba3d14c3f7a8a642
Mayank_MP5 <mayank5pande@gmail.com>
2020-05-25 15:01:49 +0530

    ML Model Giving Appropriate Predictions

--- flaskApp/flask-app.py
+++ flaskApp/flask-app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, abort
 
 # Form Code
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+
+import pickle as pkl
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
 
 # class MyForm(FlaskForm):
 #     name = StringField('name', validators=[DataRequired()])
@@ -16,6 +20,7 @@ app.config['WTF_CSRF_ENABLED'] = False
 @app.route('/submit', methods=('GET', 'POST'))
 def getAllData():
     carProfile = {}
+    # Only POST request Allowed
     if request.method == "POST":
         carProfile['buying'] = request.form["buying"]
         carProfile['maint'] = request.form["maint"]
@@ -24,7 +29,24 @@ def getAllData():
         carProfile['lug_boot'] = request.form["lug_boot"]
         carProfile['safety'] = request.form["safety"]
 
-    return render_template('sucess.html', profile=carProfile)
+        # Importing the ML model
+        model = pkl.load(open('model/DecisionTree.pickle', 'rb'))
+        # print(model)
+
+        # Building Dataframe
+        carTest = pd.DataFrame({
+            'buying': [carProfile['buying']],
+            'maint': [carProfile['maint']],
+            'doors': [carProfile['doors']],
+            'persons': [carProfile['persons']],
+            'lug_boot': [carProfile['lug_boot']],
+            'safety': [carProfile['safety']]
+        })
+        pred = model.predict(carTest)
+        carProfile["carPred"] = pred
+        return render_template('sucess.html', profile=carProfile)
+    else:
+        return abort(400)
 
 
 @app.route('/')
